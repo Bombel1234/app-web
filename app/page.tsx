032c88b728e19f03a2b020ff28e7a89b2ddf324a
test <141035353+stolarnia-eko@users.noreply.github.com>
2026-01-02 15:45:30 +0100

    up3

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,120 @@
+"use client";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
+import { auth, provider } from "@/app/lib/firebase"
+import { signInWithPopup, onAuthStateChanged, User } from "firebase/auth";
+import { FormEvent, useEffect, useState } from "react";
+import { Eye, EyeOff } from 'lucide-react'
 
-export default function Home() {
+
+export default function Login() {
+  const router = useRouter();
+
+  // 1. АВТОМАТИЧЕСКИЙ ПЕРЕХОД
+  // Если пользователь уже авторизован (вернулся на сайт), 
+  // Firebase найдет сессию и мы перекинем его на /home
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("Пользователь найден, перенаправляем...");
+        router.push("/home"); // Путь к вашей главной странице
+      }
+    });
+
+    return () => unsubscribe();
+  }, [router]);
+
+  // 2. ФУНКЦИЯ РЕГИСТРАЦИИ / ВХОДА Google
+  const handleLogin = async () => {
+    try {
+      // Firebase автоматически регистрирует нового пользователя 
+      // или входит в аккаунт существующего
+      await signInWithPopup(auth, provider);
+
+      // После успешного входа сработает onAuthStateChanged выше 
+      // и перенаправит пользователя. Но можно продублировать и здесь:
+      router.push("/home");
+    } catch (error) {
+      console.error("Ошибка при входе:", error);
+      alert("Не удалось войти через Google");
+    }
+  };
+
+  // 3 input password
+  const [showPassword, setShowPassword] = useState(false);
+  const [password, setPassword] = useState('');
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+  // 4 registration email
+  const clickEmail = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const password = formData.get("password");
+    const email = formData.get("email");
+
+    // console.log(email)
+  }
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="flex  items-center justify-center min-h-screen ">
+      <div className="w-screen bg-black-800 m-auto px-2 text-center">
+        <h1 className="text-[38px] text-center text-white font-bold mb-12">Witaj!!!</h1>
+        <button
+          onClick={handleLogin}
+          className="px-6 py-4 mb-6  bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition"
+        >
+          Wejscie przez Google
+        </button>
+        <h2 className="text-2xl text-white font-bold mt-10 mb-12">Rejestracja przez email</h2>
+        <form action="" onSubmit={clickEmail}>
+          <input type="email" name="email" placeholder="Enter email"
+            className="px-4 mb-6 text-white text-[24px] py-2 border border-gray-300 rounded-lg shadow-sm 
+                     focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent
+                     transition-all duration-200 placeholder-gray-400"
+          />
+          <div className="flex flex-col gap-2 px-2">
+            <label htmlFor="password" className="text-sm font-medium">
+              Пароль
+            </label>
+
+            <div className="relative">
+              <input
+                id="password"
+                type={showPassword ? 'text' : 'password'}
+                value={password}
+                name="email"
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full px-4 py-2 text-white text-[24px] border rounded-lg focus:ring-2 focus:ring-blue-500 outline-none pr-10"
+                placeholder="Введите пароль"
+              />
+              <button
+                type="button"
+                onClick={togglePasswordVisibility}
+                tabIndex={-1}
+                className="absolute text-white w-6 h-6 right-6 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700 focus:outline-none"
+                aria-label={showPassword ? "Скрыть пароль" : "Показать пароль"}
+              >
+                {showPassword ? (
+                  <EyeOff size={20} />
+                ) : (
+                  <Eye size={20} />    
+                )}
+              </button>
+            </div>
+          </div>
+          <button
+            
+            type="submit"
+            className="px-10 py-2 mt-20 text-[22px]  bg-red-500 text-white rounded-lg hover:bg-red-700 transition"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+            Registration
+          </button>
+        </form>
+      </div>
+
+
     </div>
   );
 }
